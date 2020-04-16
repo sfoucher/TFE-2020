f0c5ad8a0cae6078339d91754150a6514b9f1e4e
Alexandre-Delplanque <61557006+Alexandre-Delplanque@users.noreply.github.com>
2020-04-16 09:31:14 +0200

    Add files via upload

--- Pre-processing/analyse.py
+++ Pre-processing/analyse.py
@@ -1,9 +1,9 @@
-from coco_analyse import cocoanalyse
+from COCOmgmt import COCOmgmt
 import argparser
 
 args = argparser.parse_cmdline_args('analyse')
 
-analyse = cocoanalyse(args.ann_path)
+analyse = COCOmgmt(args.ann_path)
 analyse.getinfo()
 analyse.displayanndist(x_lab=args.xa_lab,y_lab=args.ya_lab,title=args.a_title,output_path=args.out_path)
 analyse.displaycatdist(x_lab=args.xc_lab,y_lab=args.yc_lab,title=args.c_title,output_path=args.out_path)
