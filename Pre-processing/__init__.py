438536f9f31778cf4b8c87e398b3dd9210f334ab
Alexandre-Delplanque <61557006+Alexandre-Delplanque@users.noreply.github.com>
2020-04-21 13:31:22 +0200

    Create __init__.py

---
+++ Pre-processing/__init__.py
@@ -0,0 +1,7 @@
+from .COCOmgmt import COCOmgmt
+from .Colabeler_to_COCO import Colabeler_to_COCO
+from .analyse import analyse
+from .argparser import argparser
+from .dataset import dataset
+from .subframes import subframes
+from .subframes_maker import subframes_maker
